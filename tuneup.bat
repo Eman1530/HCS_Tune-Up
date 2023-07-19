5ed32f1d5e841f61e9c2952a60547ffb0b501345
Eman1530 <ethanreandeau@gmail.com>
2023-07-19 12:28:36 -0700

    Update tuneup.bat

--- tuneup.bat
+++ tuneup.bat
@@ -1,5 +1,5 @@
 @echo off
-title HCS Tune-Up Script V1.0.0
+title HCS Tune-Up Script V1.1.0
 
 NET SESSION >nul 2>&1
 IF %errorlevel% EQU 0 (
@@ -8,57 +8,16 @@ IF %errorlevel% EQU 0 (
 	color 4f
 	
 	echo. ERROR: Run as administrator.
-	pause
-	goto :eof
+ Pause
 
-:: **************************************
-:: the menu interface 
-: menu
-cls
-	
-	echo.         HCS Tune-Up Script V-1.0.0
-	echo.           (Script by bit-tiger)   
-	echo.                                   
-	echo. h - help         x - exit         i - info
-	echo. ******************************************
-	echo. 1. Install tools and make HCS-Tools Folder
-	echo. 2. Install Open-Office
-	echo. 3. Install Libre-Office
-	echo. 4. Install Adobe-Acrobat-Reader
-	
-	
-	:: **************************************
-	:: menu logic
-	
-	choice /c hix1234 /n
-	cls
-	IF %errorlevel%==1 (
-		goto :help
-	) ELSE IF %errorlevel%==2 (
-		goto :info
-	) ELSE IF %errorlevel%==3 (
-		goto eof
-	) ELSE IF %errorlevel%==4 (
-		goto :install
-	) ELSE IF %errorlevel%==5 (
-		goto :oo
-	) ELSE IF %errorlevel%==6 (
-		goto :lo
-	) ELSE IF %errorlevel%==7 (
-		goto :aar
-	) ELSE echo. please pick an option
-	pause
-	goto :menu
-	
-:: **************************************
-: install
 cls
 	
 winget install -e --id Malwarebytes.Malwarebytes
 winget install -e --id Piriform.CCleaner
 winget install -e --id VideoLAN.VLC
 winget install -e --id Mozilla.Firefox
-
+winget install -e --id TheDocumentFoundation.LibreOffice
+winget install -e --id Adobe.Acrobat.Reader.64-bit
 	
 	
 	cls
@@ -77,64 +36,6 @@ winget install -e --id Mozilla.Firefox
 	move "C:\Users\Public\Desktop\Firefox.lnk" "%userprofile%/desktop"
 	move "C:\Users\Public\Desktop\Malwarebytes.lnk" %HCS%
 	move "Call_HCS_4_Help_(360)-379-4865.exe" %HCS%
-	
-	pause
-	goto :menu
-:: **************************************
-:oo
-cls
-	
-	:: Open Office
-	winget install "OpenOffice" -s winget
-	
-	cls
-	echo. Open Office installed
-	pause
-	
-	goto :menu
-:: **************************************
-:aar
-cls
-
-	:: Adobe Acrobat Reader
-	winget install "Adobe Acrobat Reader DC" -s winget
-	
-	cls
-	echo. Adobe Acrobat Reader installed
-	pause
-	
-	goto :menu
-:: **************************************
-:lo
-cls
-
-	::Libre Office
-	winget install "LibreOffice" -s winget
-
-	cls
-	echo. Libre Office installed
-	pause
-
-	goto :menu
-
-:: **************************************
-:info
-cls
-	echo. HCS Tune-Up Script V-1.1.0
-	echo. Made by bit-tiger updated by Eman1530 for HADLOCK COMPUTER SERVICES
-	
-	pause
-	goto :menu
-:: **************************************
-:help
-cls
-	echo. Option 1 will install; Malwarebytes, CCleaner, VLC, Firefox, and aero admin.
-	echo. It wll then create the HCS_Tools folder, and place links to the apps in said folder.
-	echo.
-
-	echo. Option 2 will install Open Office
-	echo. Option 3 will install Adobe Acrobat Reader
-	echo. Option 4 will install Libre Office 
 	
 	pause
 	goto :menu
